52bacb656c69befd038377ccd41ad908fb50c7b2
iv@zaoblako.com <i.a.volik@gmail.com>
2021-05-09 21:10:37 +0300

    initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cars4rent-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
